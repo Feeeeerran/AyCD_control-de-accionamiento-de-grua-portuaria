0c127e07a5ea675768685beb16b54e8118b1945e
Ferran <oheydudeman@gmail.com>
2025-10-22 22:44:40 -0300

    Actualizado, controladores funcionando y modelo completo con controladores incluidos

--- matlab/controladores/parametros_controladores.m
+++ matlab/controladores/parametros_controladores.m
@@ -1,24 +1,23 @@
 clc;
 
-%% Auxiliares
-% Se contempla la F_hw dentro del desarrollo del controlador, implicando
-% una variable m_l que varia segun el tipo de container. Se toma para este
-% valor la media entre M_cmax y M_cmin sumado al M_s (spreader)
-m_aux = M_s + (M_cmax + M_cmin) / 2;
-
-%% Hoist - Controlador PID
+%% Hoist - Controlador PID para el sistema de izaje
 
 % Se desarrolla el modelo dinamico, descartando T_hb, T_hEb y T_hdl se
-% obtiene T_hm = Jh_eq' ddl_h + bh_eq' dl_h
+% obtiene T_hm = Jh_eq'(TLK) ddl_h + bh_eq' dl_h
+
+% Siendo Jh_eq' = Jh_eq_aux + Jh_mc(TLK)
+% En esta instancia se calcula un Kp que solo tendra en cuenta a Jh_eq_aux
+% y a posteriori en Simulink se agrega Jh_mc(TLK) para que la ganancia
+% varie en funcion de la masa de la carga.
 
 % Donde
-Jh_eq_prima = ((Jh_eq * 2 * i_h) / r_hd) + (25000 * g * r_hd)/(2*i_h);
-bh_eq_prima = (bh_eq * 2 * i_h) / r_hd;
+Jh_eq_aux = ((M_s * r_hd) / (2 * i_h)) - ((Jh_eq * 2 * i_h) / r_hd);
+bh_eq_prima = (2 * bh_eq * i_h) / r_hd;
 
 
 % Se genera una funcion de transferencia para conocer los polos del sistema
 % tf(num, den)
-G_h = tf(1 , [Jh_eq_prima bh_eq_prima 0]);
+G_h = tf(1 , [Jh_eq_aux bh_eq_prima 0]);
 
 % Se encuentran los polos
 polos_h = pole(G_h);
@@ -26,22 +25,24 @@ polos_h = pole(G_h);
 disp('Polos del modelo dinamico de sistema de izaje')
 fprintf('   p1 = %f  \n   p2 = %f  \n',polos_h(1), polos_h(2));
 
-% Usando el metodo de pole placement se especifican los valores del
-% polinomio caracteristico generico
-% Siendo (s + p)(s^2 + 2 zitta w_n s + w_n^2)
-p = 100;
-zitta = 0.5;        % Factor de amortiguamiento
-T_r = 0.5;          % Rise time
-w_n = 2.5 / T_r;    % Frecuencia natural calculada por tabla
+% ==================  SINTONIA SERIE  =========================
+% w_n = 10 * polos_h(2);
+% n = 3;
+% 
+% Kp_h = n * w_n^2;
+% Ki_h = w_n^3;
+% Kd_h = n * w_n;
 
 
-% Finalmente se satisfacen las igualdades desarrolladas para calcular las
-% ganancias del controlador
+% ==================  ASIGNACION DE POLOS  ====================
+p = 25 * polos_h(2);  % Polo en eje real alejado 20 veces del polo del sistema
+zitta = 1.3;          % Factor de amortiguamiento
+T_r = 1;            % Settling time
+w_n = 3/ T_r;        % Frecuencia natural calculada por tabla
 
-Kp_h = (2 * zitta * w_n * p + w_n^2) * Jh_eq_prima;
-Ki_h = (w_n^2 * p) * Jh_eq_prima;
-Kd_h = (2 * zitta * w_n + p) * Jh_eq_prima - bh_eq_prima;
-
+Kp_h = (2 * zitta * w_n * p + w_n^2);
+Ki_h = (w_n^2 * p);
+Kd_h = (2 * zitta * w_n + p);
 
 
 disp('Ganancias del controlador de izaje')
@@ -54,13 +55,8 @@ fprintf('   Kp_h = %f\n   Ki_h = %f\n   Kd_h = %f\n\n\n',Kp_h, Ki_h, Kd_h);
 % obtiene T_tm = Jt_eq' ddx_t + bt_eq' dx_t
 
 % Donde
-Jt_eq_prima = (r_td/i_t) * m_t + (i_t/r_td) * J_tm + (1/(i_t * r_td)) * J_td;
-bt_eq_prima = (r_td/i_t) * b_t + (i_t/r_td) * b_tm + (1/(i_t * r_td)) * b_td;
-
-% Pruebas con otros valores
-% Jt_eq_prima =  Jt_eq * i_t/r_td + m_t * (r_td / i_t);
-% bt_eq_prima = bt_eq * i_t/r_td + b_t * (r_td / i_t);
-
+Jt_eq_prima = (i_t / r_td) * Jt_eq + (r_td / i_t) * m_t;
+bt_eq_prima = (i_t / r_td) * bt_eq + (r_td / i_t) * b_t;
 
 % Se genera una funcion de transferencia para conocer los polos del sistema
 % tf(num, den)
@@ -72,34 +68,38 @@ polos_t = pole(G_t);
 disp('Polos del modelo dinamico de sistema de carro')
 fprintf('   p1 = %f  \n   p2 = %f  \n',polos_t(1), polos_t(2));
 
-% Usando el metodo de pole placement se especifican los valores del
-% polinomio caracteristico generico
-% Siendo (s + p)(s^2 + 2 zitta w_n s + w_n^2)
-p = 0;
-zitta = 1;      % Factor de amortiguamiento
-T_r = 2;          % settling time
-w_n = 2 / T_r;    % Frecuencia natural calculada por tabla
+% ==================  SINTONIA SERIE  =========================
+% w_n = 100 * polos_t(2);
+% n = 3;
+% 
+% Kp_t = (n * w_n^2) * Jt_eq_prima;
+% Ki_t = (w_n^3) * Jt_eq_prima;
+% Kd_t = (n * w_n) * Jt_eq_prima - bt_eq_prima;
 
-
-% Finalmente se satisfacen las igualdades desarrolladas para calcular las
-% ganancias del controlador
+% ==================  ASIGNACION DE POLOS  ====================
+p = 0.01 * polos_t(2);  % Polo en eje real alejado 20 veces del polo del sistema
+zitta = 1.3;          % Factor de amortiguamiento
+T_r = 1;            % Settling time
+w_n = 1 / T_r;        % Frecuencia natural calculada por tabla
 
 Kp_t = (2 * zitta * w_n * p + w_n^2) * Jt_eq_prima;
 Ki_t = (w_n^2 * p) * Jt_eq_prima;
 Kd_t = (2 * zitta * w_n + p) * Jt_eq_prima - bt_eq_prima;
 
-Ki_t = 0;
+% ================== TUNE PID =================================
+% Kp_t = 814.248336359987;
+% Ki_t = 62.1556013121662;
+% Kd_t = 2666.69590556106;
 
 disp('Ganancias del controlador de carro')
 fprintf('   Kp_t = %f\n   Ki_t = %f\n   Kd_t = %f\n',Kp_t, Ki_t, Kd_t);
 
 
 %% Notas
-% El controlador del trolley esta raro, creo que al suponer x_t = x_td y
-% saltear la elasticidad del cable el controlador pierde efectividad. Si se
-% prueba particularmente con los otros modelos dinamicos funciona super
-% bien, el tema es cuando el F_tw se interpone.
+% Habria que desacoplar y tener en cuenta la elasticidad del cable en ambos
+% controladores (trolley y hoist) y evitar la suposicion donde la salida
+% del tambor es aprox al movimiento de la carga o carro.
 
-% Probablemente haya que desacoplar esa fuerza de alguna forma.
+
 
 
--- matlab/specs_sistema_control.m
+++ matlab/specs_sistema_control.m
@@ -1,4 +1,4 @@
-
+clc;
 
 % Valores de muestreo para la estructura y funciones del sistema de control
 % y proteccion por niveles
@@ -18,3 +18,16 @@ fs1 = 1 / T_s1;      % [Hz] Frecuencia de muestreo
 %% Nivel 0 - Seguridad / proteccion
 T_s0 = 20 / 1000;    % [s] Periodo de muestreo
 fs0 = 1 / T_s0;      % [Hz] Frecuencia de muestreo
+
+
+%% Limites operativos de movimiento
+
+% Traslacion del carro
+x_t_lims = [-30 50]; % [m] En x_t = 0 el carro se encuentra sobre el borde del muelle
+dx_t_lims = 4;       % [m/s] Velocidad maxima cargado o sin carga
+ddx_t_lims = 0.8;    % [m/s^2] Aceleracion maxima con y sin carga
+
+% Izaje de carga
+y_h_lims = [-20 40]; % [m] Altura de izaje (valores negativos representan dentro de barco
+dy_h_lims = [1.5 3]; % [m/s] Velocidad maxima de izaje (1) Con carga (2) Sin carga
+ddy_h_lims = 0.75;   % [m/s^2] Aceleracion maxima con y sin carga
