6a59dd9516475800df6de76dc9f55f9fa7df7eb0
Ferran <oheydudeman@gmail.com>
2025-10-29 21:03:19 -0300

    Mejoras para tener una simulacion mas rapida

--- matlab/specs_sistema_control.m
+++ matlab/specs_sistema_control.m
@@ -8,15 +8,15 @@ clc;
 % jerarquicos
 
 %% Nivel 2 - Control regulatorio
-T_s2 = 1 / 1000;     % [s] Periodo de muestreo
+T_s2 = 1 / 100;     % [s] Periodo de muestreo
 fs2 = 1 / T_s2;      % [Hz] Frecuencia de muestreo
 
 %% Nivel 1 - Control supervisor
-T_s1 = 20 / 1000;    % [s] Periodo de muestreo
+T_s1 = 20 / 100;    % [s] Periodo de muestreo
 fs1 = 1 / T_s1;      % [Hz] Frecuencia de muestreo
 
 %% Nivel 0 - Seguridad / proteccion
-T_s0 = 20 / 1000;    % [s] Periodo de muestreo
+T_s0 = 20 / 100;    % [s] Periodo de muestreo
 fs0 = 1 / T_s0;      % [Hz] Frecuencia de muestreo
 
 
